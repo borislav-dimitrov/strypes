25d47ec087901cfb34145925b934546e295de46f
borislav.dimitrov <borislav-dimitrov@outlook.com>
2022-03-01 08:01:18 +0200

    homework1 comments

--- Homeworks/homework_1_find_winner.py
+++ Homeworks/homework_1_find_winner.py
@@ -1,16 +1,24 @@
 def counting(n, m):
+    # Create list with all players
     players = [i for i in range(1, n + 1)]
+    # Index is m-1 because it is 0 based
     current_index = m - 1
 
+    # Loop until there is 1 player left
     while len(players) > 1:
         if len(players) == 2 and m % 2 == 0:
             players.remove(players[1])
         elif len(players) == 2 and m % 2 != 0:
             players.remove(players[0])
         else:
-            # print(f"Removing player {players[current_index]}")
+            # Remove the m-th player
             players.remove(players[current_index])
+            # Incrementing the current index with m - 1 because we already removed one item
+            # and the length of the list is - 1
             current_index += m - 1
+            # If the index is higher than the list length
+            # decrement it by the list length until it is smaller
+            # that way we simulate cycling through the players m times
         if current_index >= len(players):
             while current_index >= len(players):
                 current_index -= len(players)
@@ -22,4 +30,4 @@ if __name__ == '__main__':
     n = int(input("Input N:"))
     m = int(input("Input M:"))
     print(f"Winning Player is: {counting(n, m)}")
-# Answer for 8, 3 => 7
+
