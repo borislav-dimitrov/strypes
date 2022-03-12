39090c0da3fa3f6575859ee851466b7290cb6281
borislav-dimitrov <borislav-dimitrov@outlook.com>
2022-03-12 16:23:21 +0200

    hw4

--- Homeworks/homework_4_read_files.py
+++ Homeworks/homework_4_read_files.py
@@ -1,100 +1,114 @@
 import re
+from os.path import exists
 
 
-def get_index_by_word(look_for: str, list_:list[tuple])->int:
-	"""
-	Get tuple index from list
-	:param look_for: String key that we are looking for
-	:param list_: List of tuples
-	:return: index { int }
-	"""
-	for item in list_:
-		if look_for == item[0]:
-			return list_.index(item)
-	return -1
+def get_count_by_word(look_for: str, list_: list[tuple]) -> int:
+    """
+    Search for how many times has the word been found in the file
+    :param look_for: Word
+    :param list_: List to look in
+    :return: Times found in the file
+    """
+    for item in list_:
+        if look_for == item[0]:
+            return item[1]
+    return 0
 
 
-def merge_words(words_1: list[tuple], words_2: list[tuple]):
-	"""
-	Merge two word lists 
-	:param words_1: List with matched words and times used
-	:param words_2: List with matched words and times used
-	:return: Merged list
-	"""
-	merged = []
-	words_only_1 = [i[0] for i in words_1]
-	words_only_2 = [i[0] for i in words_2]
+def merge_words(words_1: list[tuple], words_2: list[tuple]) -> list:
+    """
+    Merge two word lists
+    :param words_1: List with matched words and times used
+    :param words_2: List with matched words and times used
+    :return: Merged list
+    """
+    merged = []
+    words_only_1 = [i[0] for i in words_1]
+    words_only_2 = [i[0] for i in words_2]
+    all_words = []
 
-	# match words from file 1 against file 2
-	for word in words_1:
-		if word[0] in words_only_2:
-			index = get_index_by_word(word[0], words_2)
+    for word in words_1:
+        if word[0] not in all_words:
+            all_words.append(word[0])
+    for word in words_2:
+        if word[0] not in all_words:
+            all_words.append(word[0])
 
-			if index != -1:
-				merged.append((word[0], word[1], words_2[index][1]))
-		else:
-			merged.append((word[0], word[1], 0))
+    # Match words
+    for word in all_words:
+        # For file 1
+        current_row = [word]
+        if word in words_only_1:
+            times_found1 = get_count_by_word(word, words_1)
+            current_row.append(times_found1)
+        else:
+            current_row.append(0)
+        if word in words_only_2:
+            times_found2 = get_count_by_word(word, words_2)
+            current_row.append(times_found2)
+        else:
+            current_row.append(0)
+        merged.append(current_row)
 
-	# match words from file 2 against file 1
-	for word in words_1:
-		if word[0] in words_only_2:
-			index = get_index_by_word(word[0], words_2)
-
-			if index != -1:
-				if (word[0], word[1], words_2[index][1]) not in merged:
-					merged.append((word[0], word[1], words_2[index][1]))
-		else:
-			if (word[0], word[1], 0) not in merged:
-				merged.append((word[0], word[1], 0))
-
-	return merged
-
-
-def read_file(file: str, filter_pattern: str=r'\W+'):
-	"""
-	Filter given file and return key words + count
-	:param file: File name if in same dir. If not full path
-	:param filter_pattern: Regex filter pattern
-	:return: list with matched keywords and times used
-	"""
-	all_words = {}
-	with open(file, mode="rt", encoding="utf-8") as f:
-		for line in f:
-			words = line.strip().split("#")[0]
-			words = re.split(filter_pattern, words.strip())
-			for word in words:
-				if word != "":
-					all_words[word] = all_words.get(word, 0) + 1
-
-	wc_items: list[tuple[str, int]] = list(all_words.items())
-	wc_items.sort(key=lambda t: t[1], reverse=True)
-	return wc_items
+    return merged
 
 
-def calc_difference(list_: list[tuple])->int:
-	"""
-	Calculate the difference between the two files
-	:param list_: List with tuples ( word, times in file-1, times in file-2 )
-	:return: int
-	"""
-	total_items = len(list_)
-	test = []
-	for item in list_:
-		current = item[1] - item[2]
-		if current < 0:
-			current *= -1
-		test.append(current)
+def read_file(file: str, filter_pattern: str = r'\W+'):
+    """
+    Filter given file and return key words + count
+    :param file: File name if in same dir. If not full path
+    :param filter_pattern: Regex filter pattern
+    :return: list with matched keywords and times used
+    """
+    all_words = {}
+    with open(file, mode="rt", encoding="utf-8") as f:
+        for line in f:
+            words = line.strip().split("#")[0]
+            words = re.split(filter_pattern, words.strip())
+            for word in words:
+                if word != "":
+                    all_words[word] = all_words.get(word, 0) + 1
 
-	print(test)
-	return 0
+    work_items: list[tuple[str, int]] = list(all_words.items())
+    work_items.sort(key=lambda t: t[1], reverse=True)
+    return work_items
+
+
+def calc_difference(list_: list[tuple]) -> int:
+    """
+    Calculate the difference between the two files
+    :param list_: List with tuples ( word, times in file-1, times in file-2 )
+    :return: int
+    """
+    N = len(list_)
+    total = 0
+    for item in list_:
+        current = item[1] - item[2]
+        if current < 0:
+            current *= -1
+        total += current
+
+    return round(total / N, 20)
+
 
 if __name__ == '__main__':
-	file_1_words = read_file("file_to_read_1.py")
-	file_2_words = read_file("file_to_read_2.py")
-	# TODO change the way we sort the lists
-	#	Calc the difference
-	merged = merge_words(file_1_words, file_2_words)
-	calc_difference(merged)
-	# print(calc_difference(merged))
+    file1 = input("Input file1 name: ")
+    file2 = input("Input file2 name: ")
 
-	print()
+    if not exists(file1):
+        print(f" File \"{file1}\" not found!")
+        quit()
+
+    if not exists(file2):
+        print(f" File \"{file2}\" not found!")
+        quit()
+
+    file_1_words = read_file(file1)
+    file_2_words = read_file(file2)
+
+    merged = merge_words(file_1_words, file_2_words)
+    for i in merged:
+        print(i)
+
+    difference = calc_difference(merged)
+    print(f"Степен на различност = {difference}")
