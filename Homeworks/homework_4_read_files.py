73e5c1f715083b1be5d30ef384d7340aadec725d
borislav-dimitrov <borislav-dimitrov@outlook.com>
2022-03-12 17:06:28 +0200

    hw4

--- Homeworks/homework_4_read_files.py
+++ Homeworks/homework_4_read_files.py
@@ -4,7 +4,7 @@ from os.path import exists
 
 def get_count_by_word(look_for: str, list_: list[tuple]) -> int:
     """
-    Search for how many times has the word been found in the file
+    Search for how many times the word has been found in the file
     :param look_for: Word
     :param list_: List to look in
     :return: Times found in the file
@@ -63,18 +63,21 @@ def read_file(file: str, filter_pattern: str = r'\W+'):
     all_words = {}
     with open(file, mode="rt", encoding="utf-8") as f:
         for line in f:
+            # Remove the commentaries
             words = line.strip().split("#")[0]
+            # Add the desired filter pattern
             words = re.split(filter_pattern, words.strip())
             for word in words:
                 if word != "":
                     all_words[word] = all_words.get(word, 0) + 1
 
+    # Sort descending
     work_items: list[tuple[str, int]] = list(all_words.items())
     work_items.sort(key=lambda t: t[1], reverse=True)
     return work_items
 
 
-def calc_difference(list_: list[tuple]) -> int:
+def calc_difference(list_: list[tuple]) -> float:
     """
     Calculate the difference between the two files
     :param list_: List with tuples ( word, times in file-1, times in file-2 )
@@ -87,28 +90,28 @@ def calc_difference(list_: list[tuple]) -> int:
         if current < 0:
             current *= -1
         total += current
-
-    return round(total / N, 20)
+    print(f"N = i = {N}\n")
+    return round(float(total / N), 20)
 
 
 if __name__ == '__main__':
     file1 = input("Input file1 name: ")
     file2 = input("Input file2 name: ")
-
     if not exists(file1):
-        print(f" File \"{file1}\" not found!")
+        print(f"File \"{file1}\" not found!")
         quit()
 
     if not exists(file2):
-        print(f" File \"{file2}\" not found!")
+        print(f"File \"{file2}\" not found!")
         quit()
 
     file_1_words = read_file(file1)
+    print("\nWords from file1\n", file_1_words, "\n")
     file_2_words = read_file(file2)
+    print("Words from file2\n", file_2_words, "\n")
 
     merged = merge_words(file_1_words, file_2_words)
-    for i in merged:
-        print(i)
+    print("List with mapped words from both files\n", merged, "\n")
 
     difference = calc_difference(merged)
     print(f"Степен на различност = {difference}")
