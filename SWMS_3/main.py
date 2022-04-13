da7e55eed29dc8023039b5043f1e2885d10a1aa5
borislav-dimitrov <borislav-dimitrov@outlook.com>
2022-04-13 14:16:54 +0300

    Changed logout logic from recursive to loop

--- SWMS_3/main.py
+++ SWMS_3/main.py
@@ -9,23 +9,26 @@ from view.login import Login
 def main():
     main_controller = MainController()
     main_controller.startup()
+    main_controller.logging_out = True
 
-    root = tk.Tk()
-    login = Login(root, "Login", (640, 360), main_controller, grid_rows=6, grid_cols=10)
-    main_controller.view = login
-    root.mainloop()
+    while main_controller.logging_out:
+        main_controller.logging_out = False
 
-    if main_controller.logged_user is not None:
+        # Preview Login screen
         root = tk.Tk()
-        home_view = HomeView(root, "Home", main_controller)
-        main_controller.view = home_view
+        login = Login(root, "Login", (640, 360), main_controller, grid_rows=6, grid_cols=10)
+        main_controller.view = login
         root.mainloop()
 
-    main_controller.before_exit()
+        # Make sure that we have logged user
+        if main_controller.logged_user is not None:
+            # Preview Main View
+            root = tk.Tk()
+            home_view = HomeView(root, "Home", main_controller)
+            main_controller.view = home_view
+            root.mainloop()
 
-    if main_controller.logging_out:
-        main_controller.logging_out = False
-        main()
+        main_controller.before_exit()
 
 
 if __name__ == '__main__':
